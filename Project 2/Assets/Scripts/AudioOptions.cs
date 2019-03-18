c341602bd90bddd3bfb5765f594864ffa0f2bca2
Jamie <jamielaks@gmail.com>
2019-03-17 19:03:16 -0700

    Enabled audio sliders

--- Project 2/Assets/Scripts/AudioOptions.cs
+++ Project 2/Assets/Scripts/AudioOptions.cs
@@ -1,9 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
-public class NewBehaviourScript : MonoBehaviour
+public class AudioOptions : MonoBehaviour
 {
+    public AudioMixer masterMixer;
+
+    public void SetMusic(float soundLevel)
+    {
+        masterMixer.SetFloat("musicVol", Mathf.Log(soundLevel) * 20);
+    }
+
+    public void SetSfx(float soundLevel)
+    {
+        masterMixer.SetFloat("sfxVol", Mathf.Log(soundLevel)*20);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
